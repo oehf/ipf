6495db183b2587b7d1afa9db853111ef6577197b
Martin Krasser <krasserm@github.com>
2009-06-10 11:42:17 +0000

    added tests for HL7 codec as implement in camel-hl7 version 1.6.1
    git-svn-id: http://gforge.openehealth.org/svn/ipf/trunk/ipf@338 d594d982-de57-441b-9a2b-38caaf397e83

---
+++ platform-camel/hl7/src/test/groovy/org/openehealth/ipf/platform/camel/hl7/extend/TransportRouteBuilder.groovy
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openehealth.ipf.platform.camel.hl7.extend
+
+import org.apache.camel.spring.SpringRouteBuilder
+/**
+ * @author Martin Krasser
+ */
+class TransportRouteBuilder extends SpringRouteBuilder {
+    
+    void configure() {
+
+        from('mina:tcp://127.0.0.1:8888?sync=true&codec=hl7Codec')
+            .unmarshal().ghl7()
+            .transmogrify {it.OBXNTE.OBX[5][5]}
+            .to('mock:output')
+            .transmogrify {''}
+
+        from('mina:tcp://127.0.0.1:8889?sync=true&codec=hl7Codec')
+            .unmarshal().ghl7()
+            .to('mock:output')
+            .transmogrify {''}
+
+    }
+    
+}
---
+++ platform-camel/hl7/src/test/java/org/openehealth/ipf/platform/camel/hl7/extend/Hl7TransportTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openehealth.ipf.platform.camel.hl7.extend;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openehealth.ipf.modules.hl7dsl.MessageAdapter;
+import org.openehealth.ipf.modules.hl7dsl.MessageAdapters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+/**
+ * @author Martin Krasser
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
+@ContextConfiguration(locations = { "/config/context-transport.xml" })
+public class Hl7TransportTest {
+
+    @Autowired
+    protected ProducerTemplate<Exchange> producerTemplate;
+    
+    @EndpointInject(uri="mock:output")
+    protected MockEndpoint mockOutput;
+    
+    @After
+    public void tearDown() throws Exception {
+        mockOutput.reset();
+    }
+
+    @Test
+    public void testMessage02() throws Exception {
+        String message = inputMessage("message/msg-02.hl7").toString();
+        String content = IOUtils.toString(new ClassPathResource("message/msg-02.content").getInputStream());
+        mockOutput.expectedBodiesReceived(content);
+        producerTemplate.sendBody("mina:tcp://127.0.0.1:8888?sync=true&codec=hl7Codec", message);
+        mockOutput.assertIsSatisfied();
+    }
+
+    @Test
+    public void testMessage03() throws Exception {
+        String message = inputMessage("message/msg-03.hl7").toString();
+        mockOutput.expectedBodiesReceived(message);
+        producerTemplate.sendBody("mina:tcp://127.0.0.1:8889?sync=true&codec=hl7Codec", message);
+        mockOutput.assertIsSatisfied();
+    }
+
+    private static MessageAdapter inputMessage(String resource) {
+        return MessageAdapters.load(resource);
+    }
+    
+}
