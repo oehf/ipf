6ae7fa8314502468ab7bad56ef14e5e07ef30859
Christian Ohr <ohr@github.com>
2009-10-19 12:33:40 +0000

    Added option to enforce that at least one message validation rule must be selected. For backwards compatibility, it is set to false by default
    git-svn-id: http://gforge.openehealth.org/svn/ipf/trunk/ipf@835 d594d982-de57-441b-9a2b-38caaf397e83

---
+++ modules/hl7/src/main/groovy/org/openehealth/ipf/modules/hl7/validation/model/MissingMessageRule.groovy
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.openehealth.ipf.modules.hl7.validation.model
+
+import ca.uhn.hl7v2.validation.MessageRuleimport ca.uhn.hl7v2.validation.ValidationExceptionimport ca.uhn.hl7v2.validation.ValidationExceptionimport ca.uhn.hl7v2.model.Message
+
+/**
+ * A private rule that may be used if no other message rule could be found
+ * 
+ * @author i000161
+ *
+ */
+class MissingMessageRule implements MessageRule {
+
+	String description
+	String sectionReference
+	
+	MissingMessageRule() {
+        super()
+    }
+	
+	MissingMessageRule(String description, String sectionReference) {
+        super()
+		this.description = description
+		this.sectionReference = sectionReference
+    }
+    
+    /**
+     * @see ca.uhn.hl7v2.validation.MessageRule#test(ca.uhn.hl7v2.model.Message)
+     */
+    public ValidationException[] test(Message msg){
+         [ new ValidationException("Message rule required for ${msg.eventType}^${msg.triggerEvent} (${msg.version})" +
+		   'but none could be found. Probably the message type is not allowed in this context') ] as ValidationException[]
+    }
+	
+}
