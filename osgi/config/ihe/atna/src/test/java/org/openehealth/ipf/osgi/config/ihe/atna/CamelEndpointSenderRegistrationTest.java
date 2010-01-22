550f22994338a62ef6911f2df0873e3614067601
Boris Stanojevic <stanojevic-boris@github.com>
2010-01-22 16:03:59 +0000

    Source code formatted
    git-svn-id: http://gforge.openehealth.org/svn/ipf/trunk/ipf@1029 d594d982-de57-441b-9a2b-38caaf397e83

--- osgi/config/ihe/atna/src/test/java/org/openehealth/ipf/osgi/config/ihe/atna/CamelEndpointSenderRegistrationTest.java
+++ osgi/config/ihe/atna/src/test/java/org/openehealth/ipf/osgi/config/ihe/atna/CamelEndpointSenderRegistrationTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.openehealth.ipf.osgi.config.ihe.atna;
 
 import static org.junit.Assert.*;
@@ -10,28 +25,35 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/context.xml"})
+@ContextConfiguration(locations = { "/context.xml" })
 public class CamelEndpointSenderRegistrationTest {
 
     @Autowired
     CamelEndpointSenderRegistration camelEndpointSenderListener;
 
-
     @Test
-    public void testSetAndUnset(){
+    public void testSetAndUnset() {
         CamelEndpointSender sender = new CamelEndpointSender();
         assertNotNull(camelEndpointSenderListener.getIheAuditorContext());
-        assertEquals("localhost", camelEndpointSenderListener.getIheAuditorContext().getConfig().getAuditRepositoryHost());
-        assertEquals(514, camelEndpointSenderListener.getIheAuditorContext().getConfig().getAuditRepositoryPort());
+        assertEquals("localhost", camelEndpointSenderListener
+                .getIheAuditorContext().getConfig().getAuditRepositoryHost());
+        assertEquals(514, camelEndpointSenderListener.getIheAuditorContext()
+                .getConfig().getAuditRepositoryPort());
 
         camelEndpointSenderListener.setCamelEndpointSenderService(sender, null);
-        assertEquals("0.0.0.0", camelEndpointSenderListener.getIheAuditorContext().getConfig().getAuditRepositoryHost());
-        assertEquals(0, camelEndpointSenderListener.getIheAuditorContext().getConfig().getAuditRepositoryPort());
-        assertEquals(sender, camelEndpointSenderListener.getIheAuditorContext().getSender());
+        assertEquals("0.0.0.0", camelEndpointSenderListener
+                .getIheAuditorContext().getConfig().getAuditRepositoryHost());
+        assertEquals(0, camelEndpointSenderListener.getIheAuditorContext()
+                .getConfig().getAuditRepositoryPort());
+        assertEquals(sender, camelEndpointSenderListener.getIheAuditorContext()
+                .getSender());
 
-        camelEndpointSenderListener.unsetCamelEndpointSenderService(sender, null);
-        assertEquals("localhost", camelEndpointSenderListener.getIheAuditorContext().getConfig().getAuditRepositoryHost());
-        assertEquals(514, camelEndpointSenderListener.getIheAuditorContext().getConfig().getAuditRepositoryPort());
+        camelEndpointSenderListener.unsetCamelEndpointSenderService(sender,
+                null);
+        assertEquals("localhost", camelEndpointSenderListener
+                .getIheAuditorContext().getConfig().getAuditRepositoryHost());
+        assertEquals(514, camelEndpointSenderListener.getIheAuditorContext()
+                .getConfig().getAuditRepositoryPort());
     }
 
 }
