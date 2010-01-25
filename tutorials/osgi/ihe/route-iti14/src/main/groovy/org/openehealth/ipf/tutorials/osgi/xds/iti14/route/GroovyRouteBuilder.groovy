2cc87976003eb4915d51f8f767c24aa786cda790
Boris Stanojevic <stanojevic-boris@github.com>
2010-01-25 15:23:35 +0000

    ihe osgi tutorial checked in
    git-svn-id: http://gforge.openehealth.org/svn/ipf/trunk/ipf@1034 d594d982-de57-441b-9a2b-38caaf397e83

---
+++ tutorials/osgi/ihe/route-iti14/src/main/groovy/org/openehealth/ipf/tutorials/osgi/xds/iti14/route/GroovyRouteBuilder.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openehealth.ipf.tutorials.osgi.xds.iti14.route
+
+import static org.openehealth.ipf.commons.ihe.xds.core.responses.Status.*
+
+import org.apache.camel.spring.SpringRouteBuilder
+
+import org.openehealth.ipf.commons.ihe.xds.core.responses.Response
+import org.openehealth.ipf.commons.ihe.xds.core.requests.RegisterDocumentSet
+import org.openehealth.ipf.platform.camel.core.util.Exchanges
+
+/**
+ * @author Jens Riemschneider
+ */
+public class GroovyRouteBuilder extends SpringRouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from('xds-iti14:xds-iti14-service1')
+            .validate().iti14Request()
+            .process { checkValue(it, 'service 1') }
+            .validate().iti14Response()
+            
+        from('xds-iti14:xds-iti14-service2')
+            .process { checkValue(it, 'service 2') }
+
+        from('xds-iti14:xds-iti14-service12?audit=false&allowIncompleteAudit=true')
+            .process { checkValue(it, 'service 12') }
+    
+        from('xds-iti14:xds-iti14-service13?allowIncompleteAudit=true')
+            .process { checkValue(it, 'service 13') }
+    }
+
+    def checkValue(exchange, expected) {
+        Exchanges.resultMessage(exchange).body = new Response(SUCCESS)
+    }
+}
---
+++ tutorials/osgi/ihe/route-iti8/src/main/groovy/org/openehealth/ipf/tutorials/osgi/ihe/pix/iti8/route/GroovyRouteBuilder.groovy
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openehealth.ipf.tutorials.osgi.ihe.pix.iti8.route
+
+import org.openehealth.ipf.modules.hl7.message.MessageUtils
+import org.apache.camel.Exchange
+import org.apache.camel.spring.SpringRouteBuilder
+import static org.openehealth.ipf.platform.camel.core.util.Exchanges.resultMessage
+
+
+/**
+ * @author Dmytro Rud
+ */
+class GroovyRouteBuilder extends SpringRouteBuilder {
+
+     void configure() throws Exception {
+
+         // normal processing without auditing
+         from('xds-iti8://0.0.0.0:8887?audit=false')
+             .process {
+                 resultMessage(it).body = MessageUtils.ack(it.in.body.target)
+             }
+             
+         // normal processing with auditing
+         from('pix-iti8://0.0.0.0:8888')
+             .process {
+                 resultMessage(it).body = MessageUtils.ack(it.in.body.target)
+             }
+         
+         // normal processing with support for incomplete auditing
+         from('xds-iti8://0.0.0.0:8889?allowIncompleteAudit=true')
+             .process {
+                 resultMessage(it).body = MessageUtils.ack(it.in.body.target)
+             }
+         
+         // fictive route to test producer-side acceptance checking
+         from('pix-iti8://0.0.0.0:8890')
+             .process {
+                 resultMessage(it).body.MSH[9][1] = 'DOES NOT MATTER'
+                 resultMessage(it).body.MSH[9][2] = 'SHOULD FAIL IN INTERCEPTORS'
+             }
+     }
+}
