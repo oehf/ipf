59d0d77442560291519aec893b7c851f14e748b7
mkolev <me2stk@gmail.com>
2010-09-23 23:36:52 +0200

    Small changes to allow compilation with Groovy 1.7

--- modules/hl7/src/test/groovy/org/openehealth/ipf/modules/hl7/validation/DefaultValidationContextTest.groovy
+++ modules/hl7/src/test/groovy/org/openehealth/ipf/modules/hl7/validation/DefaultValidationContextTest.groovy
@@ -36,8 +36,8 @@ public class DefaultValidationContextTest extends GroovyTestCase{
 	
 	void testGetPrimitiveRules(){
 	    DefaultValidationContext r = new DefaultValidationContext();
-	    PrimitiveTypeRule rule1 = new ClosurePrimitiveTypeRule() {}
-	    PrimitiveTypeRule rule2 = new ClosurePrimitiveTypeRule() {}
+	    PrimitiveTypeRule rule1 = new ClosurePrimitiveTypeRule({})
+	    PrimitiveTypeRule rule2 = new ClosurePrimitiveTypeRule({})
 	    r.addPrimitiveRule('2.5', 'ST', rule1)
 	    r.addPrimitiveRule('2.5', 'ST', rule2)
 	    assert r.getPrimitiveRules('2.5', 'ST', null)[0] == rule1
@@ -47,8 +47,8 @@ public class DefaultValidationContextTest extends GroovyTestCase{
 	
 	void testGetMessageRules(){
 	    DefaultValidationContext r = new DefaultValidationContext();
-	    MessageRule rule1 = new ClosureMessageRule() {}
-	    MessageRule rule2 = new ClosureMessageRule() {}
+	    MessageRule rule1 = new ClosureMessageRule ({})
+	    MessageRule rule2 = new ClosureMessageRule ({})
 	    r.addMessageRule('2.5', 'ADT', 'A01', rule1)
 	    r.addMessageRule('2.5', 'ADT', 'A01', rule2)
 	    assert r.getMessageRules('2.5', 'ADT', 'A01')[0] == rule1
@@ -58,8 +58,8 @@ public class DefaultValidationContextTest extends GroovyTestCase{
 	
 	void testGetMissingMessageRule(){
 	    DefaultValidationContext r = new DefaultValidationContext(requireMessageRule:true);
-	    MessageRule rule1 = new ClosureMessageRule() {}
-	    MessageRule rule2 = new ClosureMessageRule() {}
+	    MessageRule rule1 = new ClosureMessageRule({})
+	    MessageRule rule2 = new ClosureMessageRule({})
 	    r.addMessageRule('2.5', 'ADT', 'A01', rule1)
 	    r.addMessageRule('2.5', 'ADT', 'A01', rule2)
 	    assert r.getMessageRules('2.4', 'ADT', 'A01').size() == 1
@@ -69,8 +69,8 @@ public class DefaultValidationContextTest extends GroovyTestCase{
 	
 	void testGetEncodingRules(){
 	    DefaultValidationContext r = new DefaultValidationContext();
-	    EncodingRule rule1 = new ClosureEncodingRule() {}
-	    EncodingRule rule2 = new ClosureEncodingRule() {}
+	    EncodingRule rule1 = new ClosureEncodingRule({})
+	    EncodingRule rule2 = new ClosureEncodingRule({})
 	    r.addEncodingRule('2.5', 'bla', rule1)
 	    r.addEncodingRule('2.5', 'bla', rule2)
 	    assert r.getEncodingRules('2.5', 'bla')[0] == rule1
