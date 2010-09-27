1b615364e4c331c31d50f0c1c43bbaab8e8479ba
mkolev <me2stk@gmail.com>
2010-09-27 23:41:00 +0200

    updated archetype basic and more descriptive fix of the Groovy 1.7 Compile problem for DefaultValidationContextTest

--- modules/hl7/src/test/groovy/org/openehealth/ipf/modules/hl7/validation/DefaultValidationContextTest.groovy
+++ modules/hl7/src/test/groovy/org/openehealth/ipf/modules/hl7/validation/DefaultValidationContextTest.groovy
@@ -34,21 +34,30 @@ import org.openehealth.ipf.modules.hl7.parser.PipeParser
  */
 public class DefaultValidationContextTest extends GroovyTestCase{
 	
+    static checkIsNull = { String arg ->
+       arg == null
+    }
+    
 	void testGetPrimitiveRules(){
 	    DefaultValidationContext r = new DefaultValidationContext();
-	    PrimitiveTypeRule rule1 = new ClosurePrimitiveTypeRule({})
-	    PrimitiveTypeRule rule2 = new ClosurePrimitiveTypeRule({})
+	    PrimitiveTypeRule rule1 = new ClosurePrimitiveTypeRule(checkIsNull)
+	    PrimitiveTypeRule rule2 = new ClosurePrimitiveTypeRule(checkIsNull)
 	    r.addPrimitiveRule('2.5', 'ST', rule1)
 	    r.addPrimitiveRule('2.5', 'ST', rule2)
 	    assert r.getPrimitiveRules('2.5', 'ST', null)[0] == rule1
 	    assert r.getPrimitiveRules('2.5', 'ST', null)[1] == rule2
 	    assert r.getPrimitiveRules('2.4', 'ST', null).size() == 0
+       
+        assert rule1.test("not null") == false
+        assert rule1.test(null) == true
+        assert rule2.test("not null") == false
+        assert rule2.test(null) == true
 	}
 	
 	void testGetMessageRules(){
 	    DefaultValidationContext r = new DefaultValidationContext();
-	    MessageRule rule1 = new ClosureMessageRule ({})
-	    MessageRule rule2 = new ClosureMessageRule ({})
+	    MessageRule rule1 = new ClosureMessageRule (checkIsNull)
+	    MessageRule rule2 = new ClosureMessageRule (checkIsNull)
 	    r.addMessageRule('2.5', 'ADT', 'A01', rule1)
 	    r.addMessageRule('2.5', 'ADT', 'A01', rule2)
 	    assert r.getMessageRules('2.5', 'ADT', 'A01')[0] == rule1
@@ -58,8 +67,8 @@ public class DefaultValidationContextTest extends GroovyTestCase{
 	
 	void testGetMissingMessageRule(){
 	    DefaultValidationContext r = new DefaultValidationContext(requireMessageRule:true);
-	    MessageRule rule1 = new ClosureMessageRule({})
-	    MessageRule rule2 = new ClosureMessageRule({})
+	    MessageRule rule1 = new ClosureMessageRule(checkIsNull)
+	    MessageRule rule2 = new ClosureMessageRule(checkIsNull)
 	    r.addMessageRule('2.5', 'ADT', 'A01', rule1)
 	    r.addMessageRule('2.5', 'ADT', 'A01', rule2)
 	    assert r.getMessageRules('2.4', 'ADT', 'A01').size() == 1
@@ -69,8 +78,8 @@ public class DefaultValidationContextTest extends GroovyTestCase{
 	
 	void testGetEncodingRules(){
 	    DefaultValidationContext r = new DefaultValidationContext();
-	    EncodingRule rule1 = new ClosureEncodingRule({})
-	    EncodingRule rule2 = new ClosureEncodingRule({})
+	    EncodingRule rule1 = new ClosureEncodingRule(checkIsNull)
+	    EncodingRule rule2 = new ClosureEncodingRule(checkIsNull)
 	    r.addEncodingRule('2.5', 'bla', rule1)
 	    r.addEncodingRule('2.5', 'bla', rule2)
 	    assert r.getEncodingRules('2.5', 'bla')[0] == rule1
