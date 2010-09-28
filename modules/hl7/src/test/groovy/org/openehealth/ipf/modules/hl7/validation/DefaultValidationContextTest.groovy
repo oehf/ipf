aae7c11cb0b2a3fc3cff45fbb32409515cadcfb5
mkolev <me2stk@gmail.com>
2010-09-28 23:32:26 +0200

    Remove unused import

--- modules/hl7/src/test/groovy/org/openehealth/ipf/modules/hl7/validation/DefaultValidationContextTest.groovy
+++ modules/hl7/src/test/groovy/org/openehealth/ipf/modules/hl7/validation/DefaultValidationContextTest.groovy
@@ -28,6 +28,7 @@ import org.openehealth.ipf.modules.hl7.validation.model.ClosurePrimitiveTypeRule
 import org.openehealth.ipf.modules.hl7.validation.model.ClosureMessageRule
 import org.openehealth.ipf.modules.hl7.validation.model.ClosureEncodingRule
 import org.openehealth.ipf.modules.hl7.parser.PipeParser
+
 /**
  * @author Christian Ohr
  */
