69dc15bdda5138e2bb4edcb9e23cb3a2a9326a92
Boris Stanojevic <stanojevic.boris@gmail.com>
2010-11-05 11:06:27 +0100

    Configurer recursively delegates customModelClasses to its delegateFactory. Closes #30

---
+++ modules/hl7/src/test/java/org/openehealth/ipf/modules/hl7/parser/GroovyCustomModelClassFactoryTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openehealth.ipf.modules.hl7.parser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openehealth.ipf.modules.hl7.config.CustomModelClassFactoryConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/context-custom-configurer.xml",
+		"/context-custom-classes.xml" })
+public class GroovyCustomModelClassFactoryTest {
+
+    @Autowired
+    private CustomModelClassFactoryConfigurer configurer;
+    
+    @Test
+    public void testMappings() {
+        Map<String, String[]> map = configurer.getCustomModelClassFactory().getCustomModelClasses();
+        assertTrue(map.containsKey("2.5"));
+        assertTrue(map.containsKey("2.4"));
+        Class clazz = configurer.getCustomModelClassFactory()
+                .getMessageClass("MDM_T01", "2.5", false);
+        assertNotNull(clazz);
+        assertEquals("org.openehealth.ipf.modules.hl7.parser.test.hl7v2.def.v25.message.MDM_T01",
+                clazz.getCanonicalName());
+        Class clazz1 = configurer.getCustomModelClassFactory()
+                .getMessageClass("MDM_T02", "2.4", false);
+        assertNotNull(clazz1);
+        assertEquals("org.openehealth.ipf.modules.hl7.parser.groovytest.hl7v2.def.v24.message.MDM_T02",
+                clazz1.getCanonicalName());
+    }	
+}
---
+++ modules/hl7/src/test/resources/org/openehealth/ipf/modules/hl7/parser/groovytest/hl7v2/def/v24/segment/ZBE.groovy
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openehealth.ipf.modules.hl7.parser.groovytest.hl7v2.def.v24.segment
+
+import java.util.Collection
+
+import org.openehealth.ipf.modules.hl7.model.AbstractSegment
+
+
+import ca.uhn.hl7v2.HL7Exception
+import ca.uhn.hl7v2.model.Group
+import ca.uhn.hl7v2.model.Message
+import ca.uhn.hl7v2.model.v25.datatype.EI
+import ca.uhn.hl7v2.model.v25.datatype.ST
+import ca.uhn.hl7v2.model.v25.datatype.TS
+import ca.uhn.hl7v2.parser.ModelClassFactory
+import ca.uhn.log.HapiLogFactory
+
+/**
+ * The ZBE segment is intended to be used for information that details ADT
+ * movement information. Each ADT event (i.e. admission, discharge, transfer,
+ * visit) has a unique identifier to allow for updates at a later point in time.
+ * Furthermore, other medical information like diagnoses or documents can refer
+ * to this movement using the identifier as reference.
+ * <p>
+ * The segment is defined by the German Patient Management Profile and extended
+ * by the "Historic Movement" option of the IHE ITI Supplement Patient
+ * Administration Framework (PAM) Integration Profile.
+ * 
+ * This custom segment is compiled and loaded at runtime (unlike Java custom
+ * segments, which are already available as .class file. Take care to load required
+ * custom types in the same way. For this purpose the HapiModelExtension provides
+ * addType extension methods, that take a String instead of a Class as
+ * first parameter.  
+ *
+ * @author Christian Ohr
+ */
+@SuppressWarnings("serial")
+public class ZBE extends AbstractSegment {
+
+	/**
+	 * @param parent
+	 * @param factory
+	 */
+	public ZBE(Group parent, ModelClassFactory factory) {
+		super(parent, factory)
+		Message message = getMessage()
+		try {
+			add(EI.class, true, 0, 999, [ message ] as Object[], null)
+			add(TS.class, true, 1, 26, [ message ] as Object[], null)
+			add(TS.class, false, 1, 26, [ message ] as Object[], null)
+			add(ST.class, true, 1, 10, [ message ] as Object[], null)
+		} catch (HL7Exception he) {
+			HapiLogFactory.getHapiLog(this.getClass()).error(
+					"Can't instantiate " + this.getClass().getName(), he)
+		}
+	}
+
+	/**
+	 * Returns movement ID (ZBE-1).
+	 *
+	 * @param rep index of repeating field
+	 * @return movement ID
+	 */
+	public EI getMovementID(int rep) {
+		return getTypedField(1, rep)
+	}
+
+	/**
+	 * Returns movement IDs (ZBE-1).
+	 *
+	 * @return movement IDs
+	 */
+	public EI[] getMovementID() {
+		Collection<EI> result = getTypedField(1)
+		return (EI[]) result.toArray(new EI[result.size()])
+	}
+
+	/**
+	 * Returns movement start date (ZBE-2).
+	 *
+	 * @return movement start date (required)
+	 */
+	public TS getStartMovementDateTime() {
+		return getTypedField(2, 0)
+	}
+
+	/**
+	 * Returns movement end date (ZBE-3).
+	 *
+	 * @return movement end date (optional)
+	 */
+	public TS getStartMovementEndTime() {
+		return getTypedField(3, 0)
+	}
+
+	/**
+	 * Returns movement action (ZBE-4).
+	 *
+	 * @return movement action (required, one of INSERT, DELETE, UPDATE, REFERENCE)
+	 */
+	public ST getAction() {
+		return getTypedField(4, 0)
+	}
+
+}
