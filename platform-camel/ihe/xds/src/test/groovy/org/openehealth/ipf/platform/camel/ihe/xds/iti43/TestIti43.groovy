33f19614da674ec8463bd6f61c2391c430016d87
Dmytro Rud <unixoid@web.de>
2011-03-25 18:48:49 +0100

    Logging interceptors for WS-based transactions.  Differences between them and standard CXF ones: * support for multipart messages with attachments * better pretty-printing * configurable titles of log items

--- platform-camel/ihe/xds/src/test/groovy/org/openehealth/ipf/platform/camel/ihe/xds/iti43/TestIti43.groovy
+++ platform-camel/ihe/xds/src/test/groovy/org/openehealth/ipf/platform/camel/ihe/xds/iti43/TestIti43.groovy
@@ -30,7 +30,10 @@ import static org.openehealth.ipf.commons.ihe.xds.core.responses.Status.SUCCESS
  * @author Jens Riemschneider
  */
 class TestIti43 extends StandardTestContainer {
-    def SERVICE1 = "xds-iti43://localhost:${port}/xds-iti43-service1"
+    def SERVICE1 = "xds-iti43://localhost:${port}/xds-iti43-service1" +
+            '?inInterceptors=#clientInPayloadLoggingInterceptor' +
+            '&outInterceptors=#clientOutPayloadLoggingInterceptor,#outStreamSubstituteInterceptor'
+
     def SERVICE2 = "xds-iti43://localhost:${port}/xds-iti43-service2"
 
     def SERVICE2_ADDR = "http://localhost:${port}/xds-iti43-service2"
