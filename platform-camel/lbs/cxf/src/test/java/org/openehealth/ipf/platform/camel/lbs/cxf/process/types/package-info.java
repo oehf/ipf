b49eb07a3237b588d81e8e0091d1211f3854c0b7
Dmytro Rud <unixoid@web.de>
2012-08-21 18:34:36 +0200

    remove deprecated LBS and Eventing Framework

--- platform-camel/lbs/cxf/src/test/java/org/openehealth/ipf/platform/camel/lbs/cxf/process/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://cxf.process.lbs.camel.platform.ipf.openehealth.org/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.openehealth.ipf.platform.camel.lbs.cxf.process.types;
--- tutorials/lbs/src/main/java/org/openehealth/tutorial/imagebin/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://tutorial.openehealth.org/imagebin/types/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.openehealth.tutorial.imagebin.types;
