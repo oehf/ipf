3716818826df5b455a0d5d39f080ee6ce61c867b
Boris Stanojevic <stanojevic.boris@gmail.com>
2013-02-25 13:44:14 +0100

    #15: ITI-62 wsdl + camel component initial commit

---
+++ platform-camel/ihe/xds/src/main/java/org/openehealth/ipf/platform/camel/ihe/xds/iti62/Iti62Service.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openehealth.ipf.platform.camel.ihe.xds.iti62;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.openehealth.ipf.commons.ihe.xds.core.XdsJaxbDataBinding;
+import org.openehealth.ipf.commons.ihe.xds.core.responses.ErrorCode;
+import org.openehealth.ipf.commons.ihe.xds.core.responses.Response;
+import org.openehealth.ipf.commons.ihe.xds.core.stub.ebrs30.lcm.RemoveObjectsRequest;
+import org.openehealth.ipf.commons.ihe.xds.core.stub.ebrs30.rs.RegistryResponseType;
+import org.openehealth.ipf.commons.ihe.xds.iti62.Iti62PortType;
+import org.openehealth.ipf.platform.camel.core.util.Exchanges;
+import org.openehealth.ipf.platform.camel.ihe.ws.AbstractWebService;
+import org.openehealth.ipf.platform.camel.ihe.xds.core.converters.EbXML30Converters;
+
+/**
+ * Service implementation for the IHE ITI-62 transaction (Register Document Set).
+ * <p>
+ * This implementation delegates to a Camel consumer by creating an exchange.
+ *
+ * @author Boris Stanojevic
+ */
+@Slf4j
+public class Iti62Service extends AbstractWebService implements Iti62PortType {
+
+    @Override
+    public RegistryResponseType documentRegistryDeleteDocumentSet(RemoveObjectsRequest body) {
+        Exchange result = process(body, XdsJaxbDataBinding.getMap(body), ExchangePattern.InOut);
+        Exception exception = Exchanges.extractException(result);
+        if (exception != null) {
+            log.debug("ITI-62 service failed", exception);
+            Response errorResponse = new Response(exception, ErrorCode.REGISTRY_METADATA_ERROR, ErrorCode.REGISTRY_ERROR, null);
+            return EbXML30Converters.convert(errorResponse);
+        }
+
+        return Exchanges.resultMessage(result).getBody(RegistryResponseType.class);
+    }
+}
