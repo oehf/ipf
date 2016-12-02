11ed3e246459746089fdb0c5929713fa5ba084c0
ohr <christian.ohr@gmail.com>
2016-12-02 09:13:58 +0100

    #115: fix package naming in boot modules

--- boot/ipf-xds-spring-boot-starter/src/main/java/org/openehealth/ipf/boot/xds/CachingAsynchronyCorrelator.java
+++ boot/ipf-xds-spring-boot-starter/src/main/java/org/openehealth/ipf/boot/xds/CachingAsynchronyCorrelator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openehealth.ipf.boot;
+package org.openehealth.ipf.boot.xds;
 
 import org.openehealth.ipf.commons.ihe.ws.correlation.AsynchronyCorrelator;
 import org.openehealth.ipf.commons.ihe.ws.cxf.audit.WsAuditDataset;
