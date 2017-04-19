1d3951bd091904536a0ca7ed46e6472594276627
Dmytro Rud <dmytro.rud@gmail.com>
2017-04-19 19:21:33 +0200

    moved XML routines to a better location

--- commons/xml/src/main/java/org/openehealth/ipf/commons/xml/XmlUtils.java
+++ commons/xml/src/main/java/org/openehealth/ipf/commons/xml/XmlUtils.java
@@ -15,16 +15,23 @@
  */
 package org.openehealth.ipf.commons.xml;
 
+import lombok.extern.slf4j.Slf4j;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import java.io.StringReader;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Various XML utilities.
+ *
  * @author Dmytro Rud
  */
+@Slf4j
 abstract public class XmlUtils {
 
     private static final Pattern ROOT_ELEMENT_PATTERN = Pattern.compile(
@@ -42,31 +49,116 @@ abstract public class XmlUtils {
 
     /**
      * Creates an XML Source from the given XML String.
-     * @param s
-     *      XML String.
-     * @return
-     *      XML Source.
+     *
+     * @param s XML String.
+     * @return XML Source.
      */
     public static Source source(String s) {
         return new StreamSource(new StringReader(s));
     }
 
 
-   /**
-    * Returns local name of the root element of the XML document represented
-    * by the given string, or <code>null</code>, when the given string does
-    * not contain valid XML.
-    * @param s
-    *       XML string.
-    * @return
-    *       root element local name, or <code>null</code> when it could not be determined.
-    */
-   public static String rootElementName(String s) {
-       if (s == null) {
-           return null;
-       }
-       Matcher matcher = ROOT_ELEMENT_PATTERN.matcher(s);
-       return (matcher.find() && (matcher.start() == 0)) ? matcher.group(1) : null;
-   }
+    /**
+     * Returns local name of the root element of the XML document represented
+     * by the given string, or <code>null</code>, when the given string does
+     * not contain valid XML.
+     *
+     * @param s XML string.
+     * @return root element local name, or <code>null</code> when it could not be determined.
+     */
+    public static String rootElementName(String s) {
+        if (s == null) {
+            return null;
+        }
+        Matcher matcher = ROOT_ELEMENT_PATTERN.matcher(s);
+        return (matcher.find() && (matcher.start() == 0)) ? matcher.group(1) : null;
+    }
+
+
+    /**
+     * Searches for the first sub-element of the given XML element, which has
+     * the given local name and whose namespace belongs to the given set.
+     *
+     * @param root            an XML element whose children will be iterated, null values are allowed
+     * @param nsUris          a set of namespace URIs the wanted element can belong to
+     * @param wantedLocalName local name of the wanted element
+     * @return corresponding child element or <code>null</code> when none found
+     */
+    public static Element getElementNS(
+            Element root,
+            Set<String> nsUris,
+            String wantedLocalName) {
+        if (root == null) {
+            return null;
+        }
+
+        Node node = root.getFirstChild();
+        while (node != null) {
+            if ((node instanceof Element) &&
+                    nsUris.contains(node.getNamespaceURI()) &&
+                    node.getLocalName().equals(wantedLocalName)) {
+                return (Element) node;
+            }
+
+            node = node.getNextSibling();
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Extracts the given XML element from the given XML document.
+     * <p>
+     * Notes:
+     * <ul>
+     *     <li>Only non-empty elements can be found, the form
+     *          <tt>&lt;prefix:elementName (attr="value")* /&gt;</tt> is not supported.</li>
+     *     <li>When multiple elements with the given local name are present,
+     *          the last one will be returned.</li>
+     * </ul>
+     *
+     * @param document
+     *      XML document as String.
+     * @param elementName
+     *      XML local element name.
+     * @return
+     *      XML element as String, or <code>null</code> when no element could be extracted.
+     */
+    public static String extractNonEmptyElement(String document, String elementName) {
+
+        // ... <prefix:elementName attr1="abcd"> ... </prefix:elementName> ...
+        //     3                                     2        1
+
+        try {
+            int pos1 = document.lastIndexOf(elementName + '>');
+            if (pos1 < 0) {
+                log.warn("Cannot find end of the closing tag of {}", elementName);
+                return null;
+            }
+
+            int pos2 = document.lastIndexOf('<', pos1 - 1);
+            if (pos2 < 0) {
+                log.warn("Cannot find start of the closing tag of {}", elementName);
+                return null;
+            }
+
+            StringBuilder sb = new StringBuilder().append('<');
+            if (pos1 - pos2 > 2) {
+                sb.append(document, pos2 + 2, pos1 - 1).append(':');
+            }
+            int pos3 = document.indexOf(sb.append(elementName).toString());
+            if (pos3 < 0) {
+                log.warn("Cannot find start of the opening tag of {}", elementName);
+                return null;
+            }
+
+            return document.substring(pos3, pos1 + elementName.length() + 1);
+
+        } catch (Exception e) {
+            log.error("Could not extract element" + elementName, e);
+            return null;
+        }
+    }
 
 }
