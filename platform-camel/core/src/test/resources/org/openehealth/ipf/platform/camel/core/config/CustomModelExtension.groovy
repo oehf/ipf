0240e44199a991c972751a8f7e242f6e842805ce
Christian Ohr <christian.ohr@gmail.com>
2017-10-20 12:44:33 +0200

    Remove very legacy metaclass extension classes

--- platform-camel/core/src/test/resources/org/openehealth/ipf/platform/camel/core/config/CustomModelExtension.groovy
+++ platform-camel/core/src/test/resources/org/openehealth/ipf/platform/camel/core/config/CustomModelExtension.groovy
@@ -15,18 +15,30 @@
  */
 package org.openehealth.ipf.platform.camel.core.config
 
-
 import org.apache.camel.builder.RouteBuilder
-import org.openehealth.ipf.commons.core.extend.config.Extension
+import org.openehealth.ipf.commons.core.extend.config.DynamicExtension
+
 /**
  * @author Martin Krasser
  */
-class CustomModelExtension implements Extension{
-    
-    static extensions = {
-            
-        RouteBuilder.metaClass.direct = { String endpointName ->
-            delegate.from('direct:' + endpointName)
-        }
+class CustomModelExtension implements DynamicExtension {
+
+    static RouteBuilder direct(RouteBuilder delegate, String endpointName) {
+        delegate.from('direct:' + endpointName)
+    }
+
+    @Override
+    String getModuleName() {
+        return "TestCustomExtension"
+    }
+
+    @Override
+    String getModuleVersion() {
+        return "1.0.0"
+    }
+
+    @Override
+    boolean isStatic() {
+        return true
     }
 }
