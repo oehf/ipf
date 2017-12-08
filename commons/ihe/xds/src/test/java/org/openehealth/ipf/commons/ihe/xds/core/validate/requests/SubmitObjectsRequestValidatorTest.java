15688642484b35f4892183946ec768a4cdceab99
Dmytro Rud <dmytro.rud@gmail.com>
2017-12-08 10:30:42 +0100

    #179: fixed validation of nodeRepresentation

--- commons/ihe/xds/src/test/java/org/openehealth/ipf/commons/ihe/xds/core/validate/requests/SubmitObjectsRequestValidatorTest.java
+++ commons/ihe/xds/src/test/java/org/openehealth/ipf/commons/ihe/xds/core/validate/requests/SubmitObjectsRequestValidatorTest.java
@@ -237,15 +237,53 @@ public class SubmitObjectsRequestValidatorTest {
         ebXML.getExtrinsicObjects().get(0).getClassifications().get(0).setClassifiedObject("folder01");
         expectFailure(WRONG_CLASSIFIED_OBJ, ebXML);
     }
-    
-    @Test    
-    public void testWrongNodeRepresentation() {
-        EbXMLProvideAndRegisterDocumentSetRequest ebXML = transformer.toEbXML(request);
-        ebXML.getExtrinsicObjects().get(0).getClassifications().get(0).setNodeRepresentation(null);
-        expectFailure(WRONG_NODE_REPRESENTATION, ebXML);
+
+    private static boolean isAuthorClassification(EbXMLClassification classification) {
+        return classification.getClassificationScheme().equals(Vocabulary.DOC_ENTRY_AUTHOR_CLASS_SCHEME) ||
+               classification.getClassificationScheme().equals(Vocabulary.SUBMISSION_SET_AUTHOR_CLASS_SCHEME);
     }
-    
-    @Test    
+
+    @Test
+    public void testRequiredNodeRepresentationIsNull() {
+        EbXMLProvideAndRegisterDocumentSetRequest ebXML = transformer.toEbXML(request);
+        ebXML.getExtrinsicObjects().get(0).getClassifications().stream()
+                .filter(x -> !isAuthorClassification(x))
+                .findAny()
+                .get()
+                .setNodeRepresentation(null);
+        expectFailure(NODE_REPRESENTATION_MISSING, ebXML);
+    }
+
+    @Test
+    public void testRequiredNodeRepresentationIsEmpty() {
+        EbXMLProvideAndRegisterDocumentSetRequest ebXML = transformer.toEbXML(request);
+        ebXML.getExtrinsicObjects().get(0).getClassifications().stream()
+                .filter(x -> !isAuthorClassification(x))
+                .findAny()
+                .get()
+                .setNodeRepresentation("");
+        expectFailure(NODE_REPRESENTATION_MISSING, ebXML);
+    }
+
+    @Test
+    public void testProhibitedNodeRepresentationIsNotEmpty() {
+        EbXMLProvideAndRegisterDocumentSetRequest ebXML = transformer.toEbXML(request);
+        EbXMLClassification classification = ebXML.getExtrinsicObjects().get(0).getClassifications().stream()
+                .filter(x -> isAuthorClassification(x))
+                .findAny()
+                .get();
+
+        classification.setNodeRepresentation(null);
+        validator.validate(ebXML, ITI_42);
+
+        classification.setNodeRepresentation("");
+        validator.validate(ebXML, ITI_42);
+
+        classification.setNodeRepresentation("abcd");
+        expectFailure(NODE_REPRESENTATION_PROHIBITED, ebXML);
+    }
+
+    @Test
     public void testCXTooManyComponents() {
         EbXMLProvideAndRegisterDocumentSetRequest ebXML = transformer.toEbXML(request);
         ebXML.getExtrinsicObjects().get(0).getExternalIdentifiers().get(0).setValue("rotfl^^^^^^^^^lol");
