1100773c7ccc8e5aa9558f69ddadf5843ce3df43
Christian Ohr <christian.ohr@gmail.com>
2018-06-28 10:02:36 +0200

    remove dead code

--- archetypes/basic/src/main/resources/archetype-resources/src/main/groovy/SampleModelExtensionModule.groovy
+++
@@ -1,13 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-package ${package}
-
-import org.apache.camel.model.ProcessorDefinition
-class SampleModelExtensionModule {
-      
-     static ProcessorDefinition reverse(ProcessorDefinition self) {
-         self.transmogrify { it.reverse() } 
-     }
-     
-}
--- tutorials/ref/start-console.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem --------------------------------------------------------
-rem  Start JConsole with flow manager jar containing DTOs
-rem --------------------------------------------------------
-jconsole -J-Djava.class.path="%JAVA_HOME%/lib/jconsole.jar";"%JAVA_HOME%/lib/tools.jar";lib/ipf-commons-flow-${project.version}.jar
