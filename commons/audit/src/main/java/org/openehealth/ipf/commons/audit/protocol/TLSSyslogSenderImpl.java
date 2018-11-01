baec32f9fe4e7aadafbfab51138a28c929fae5e0
Ralf Steppacher <ralf.steppacher@swisscom.com>
2018-11-01 15:57:02 +0100

    Read-before-write safeguard of the TLS socket connection. Default behavior is to not safeguard the write with a read (resembles behavior of previous implementation)

--- commons/audit/src/main/java/org/openehealth/ipf/commons/audit/protocol/TLSSyslogSenderImpl.java
+++ commons/audit/src/main/java/org/openehealth/ipf/commons/audit/protocol/TLSSyslogSenderImpl.java
@@ -15,6 +15,19 @@
  */
 package org.openehealth.ipf.commons.audit.protocol;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 import org.openehealth.ipf.commons.audit.AuditContext;
 import org.openehealth.ipf.commons.audit.AuditException;
@@ -22,30 +35,19 @@ import org.openehealth.ipf.commons.audit.utils.AuditUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.net.SocketFactory;
-import javax.net.ssl.SSLSocketFactory;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.net.SocketException;
-import java.nio.charset.StandardCharsets;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
-
 /**
- * Simple client implementation of RFC 5425 TLS syslog transport
- * for sending audit messages to an Audit Record Repository
- * that implements TLS syslog.
+ * Simple client implementation of RFC 5425 TLS syslog transport for sending
+ * audit messages to an Audit Record Repository that implements TLS syslog.
  * Multiple messages may be sent over the same socket.
  * <p>
- * Designed to run in a standalone mode
- * and is not dependent on any context or configuration.
+ * Designed to run in a standalone mode and is not dependent on any context or
+ * configuration.
  * <p>
  * <p>
- * Note that this implementation disobeys the ATNA specification saying,
- * that the Secure Application, Secure Node, or Audit Record Forwarder is unable to send the
- * message to the Audit Record Repository, then the actor shall store the audit record
- * locally and send it when it is able.
+ * Note that this implementation disobeys the ATNA specification saying, that
+ * the Secure Application, Secure Node, or Audit Record Forwarder is unable to
+ * send the message to the Audit Record Repository, then the actor shall store
+ * the audit record locally and send it when it is able.
  * </p>
  *
  * @author Lawrence Tarbox, Derived from code written by Matthew Davis of IBM.
@@ -55,8 +57,11 @@ import java.util.concurrent.atomic.AtomicReference;
 public class TLSSyslogSenderImpl extends RFC5424Protocol implements AuditTransmissionProtocol {
 
     private static final Logger LOG = LoggerFactory.getLogger(TLSSyslogSenderImpl.class);
-    private AtomicReference<Socket> socket = new AtomicReference<>();
-    private SocketFactory socketFactory = SSLSocketFactory.getDefault();
+    private static final int MIN_SO_TIMEOUT = 1;
+
+    private final AtomicReference<Socket> socket = new AtomicReference<>();
+    private final SocketFactory socketFactory;
+    private final SocketTestPolicy socketTestPolicy;
 
     /**
      * Constructor which uses default values for all parameters.
@@ -66,10 +71,23 @@ public class TLSSyslogSenderImpl extends RFC5424Protocol implements AuditTransmi
     }
 
     /**
-     * @param socketFactory SSL socket factory to be used for creating the TCP socket.
+     * @param socketFactory SSL socket factory to be used for creating the TCP
+     *                      socket.
      */
     public TLSSyslogSenderImpl(SSLSocketFactory socketFactory) {
-        this(AuditUtils.getLocalHostName(), AuditUtils.getProcessId(), socketFactory);
+        this(AuditUtils.getLocalHostName(), AuditUtils.getProcessId(), socketFactory,
+                SocketTestPolicy.DONT_TEST_POLICY);
+    }
+
+    /**
+     * 
+     * @param socketFactory    SSL socket factory to be used for creating the TCP
+     *                         socket.
+     * @param socketTestPolicy Determining if and when to test the socket for a
+     *                         connection close/reset
+     */
+    public TLSSyslogSenderImpl(SSLSocketFactory socketFactory, SocketTestPolicy socketTestPolicy) {
+        this(AuditUtils.getLocalHostName(), AuditUtils.getProcessId(), socketFactory, socketTestPolicy);
     }
 
     /**
@@ -78,16 +96,35 @@ public class TLSSyslogSenderImpl extends RFC5424Protocol implements AuditTransmi
      */
     public TLSSyslogSenderImpl(String sendingHost, String sendingProcess) {
         super(sendingHost, sendingProcess);
+        this.socketFactory = SSLSocketFactory.getDefault();
+        this.socketTestPolicy = SocketTestPolicy.DONT_TEST_POLICY;
     }
 
     /**
      * @param sendingHost    value of the SYSLOG header "HOSTNAME"
      * @param sendingProcess value of the SYSLOG header "APP-NAME"
-     * @param socketFactory  SSL socket factory to be used for creating the TCP socket.
+     * @param socketFactory  SSL socket factory to be used for creating the TCP
+     *                       socket.
      */
     public TLSSyslogSenderImpl(String sendingHost, String sendingProcess, SSLSocketFactory socketFactory) {
         super(sendingHost, sendingProcess);
         this.socketFactory = Objects.requireNonNull(socketFactory);
+        this.socketTestPolicy = SocketTestPolicy.DONT_TEST_POLICY;
+    }
+
+    /**
+     * @param sendingHost      value of the SYSLOG header "HOSTNAME"
+     * @param sendingProcess   value of the SYSLOG header "APP-NAME"
+     * @param socketFactory    SSL socket factory to be used for creating the TCP
+     *                         socket.
+     * @param socketTestPolicy Determining if and when to test the socket for a
+     *                         connection close/reset
+     */
+    public TLSSyslogSenderImpl(String sendingHost, String sendingProcess, SSLSocketFactory socketFactory,
+            SocketTestPolicy socketTestPolicy) {
+        super(sendingHost, sendingProcess);
+        this.socketFactory = Objects.requireNonNull(socketFactory);
+        this.socketTestPolicy = socketTestPolicy;
     }
 
     @Override
@@ -96,7 +133,8 @@ public class TLSSyslogSenderImpl extends RFC5424Protocol implements AuditTransmi
     }
 
     private Socket getSocket(AuditContext auditContext) {
-        if (socket.get() == null) socket.compareAndSet(null, getTLSSocket(auditContext));
+        if (socket.get() == null)
+            socket.compareAndSet(null, getTLSSocket(auditContext));
         return socket.get();
     }
 
@@ -106,21 +144,22 @@ public class TLSSyslogSenderImpl extends RFC5424Protocol implements AuditTransmi
             for (String auditMessage : auditMessages) {
                 byte[] msgBytes = getTransportPayload(auditContext.getSendingApplication(), auditMessage);
                 byte[] syslogFrame = String.format("%d ", msgBytes.length).getBytes();
-                LOG.debug("Auditing to {}:{}",
-                        auditContext.getAuditRepositoryAddress().getHostAddress(),
+                LOG.debug("Auditing to {}:{}", auditContext.getAuditRepositoryAddress().getHostAddress(),
                         auditContext.getAuditRepositoryPort());
                 LOG.trace("{}", new String(msgBytes, StandardCharsets.UTF_8));
                 try {
                     doSend(auditContext, syslogFrame, msgBytes);
-                } catch (SocketException e) {
+                } catch (SocketException | SocketTimeoutException e) {
                     try {
                         LOG.info("Failed to use existing TLS socket. Will create a new connection and retry.");
+                        closeSocket(socket.get());
                         socket.set(null);
                         doSend(auditContext, syslogFrame, msgBytes);
                     } catch (Exception exception) {
                         LOG.error("Failed to audit using new TLS socket, giving up - this audit message will be lost.");
+                        closeSocket(socket.get());
                         socket.set(null);
-                        // rethrow the exception so caller knows what happened
+                        // re-throw the exception so caller knows what happened
                         throw exception;
                     }
                 }
@@ -131,31 +170,180 @@ public class TLSSyslogSenderImpl extends RFC5424Protocol implements AuditTransmi
     @Override
     public void shutdown() {
         if (socket.get() != null) {
-            try {
-                // TODO could wait until everything is sent
-                socket.get().close();
-            } catch (IOException ignored) {
-            }
+            // TODO could wait until everything is sent
+            closeSocket(socket.get());
         }
     }
 
-    private synchronized void doSend(AuditContext auditContext, byte[] syslogFrame, byte[] msgBytes) throws IOException {
-        Socket socket = getSocket(auditContext);
+    private synchronized void doSend(AuditContext auditContext, byte[] syslogFrame, byte[] msgBytes)
+            throws IOException {
+        final Socket socket = getSocket(auditContext);
+
+        if (socketTestPolicy.isBeforeWrite()) {
+            LOG.debug("Testing whether socket connection is alive and well before attempting to write.");
+            if (!isSocketConnectionAlive(socket)) {
+                closeSocket(socket);
+                // FIXME: Usage of exceptions for control flow is an anti-pattern
+                throw new FastSocketException(
+                        "Read-test before write operation determined that the socket connection is dead.");
+            }
+            LOG.debug("Socket connection is confirmed alive. Now writing out ATNA record.");
+        }
+
         OutputStream out = socket.getOutputStream();
         out.write(syslogFrame);
         out.write(msgBytes);
         out.flush();
-    }
 
-    private Socket getTLSSocket(AuditContext auditContext) {
-        try {
-            return socketFactory.createSocket(auditContext.getAuditRepositoryAddress(), auditContext.getAuditRepositoryPort());
-        } catch (IOException e) {
-            throw new AuditException(String.format("Could not establish TLS connection to %s:%d",
-                    auditContext.getAuditRepositoryAddress().getHostAddress(),
-                    auditContext.getAuditRepositoryPort()), e);
+        LOG.debug("ATNA record has been written out.");
+
+        if (socketTestPolicy.isAfterWrite()) {
+            LOG.debug(
+                    "Testing whether socket connection is alive and well after write to confirm the write operation.");
+            if (!isSocketConnectionAlive(socket)) {
+                closeSocket(socket);
+                // FIXME: Usage of exceptions for control flow is an anti-pattern
+                throw new FastSocketException(
+                        "Read-test after write operation determined that the socket connection is dead.");
+            }
+            LOG.debug("Socket connection is confirmed alive. Assuming write operation has succeeded.");
         }
     }
 
+    private Socket getTLSSocket(AuditContext auditContext) {
+        final SSLSocket socket;
+        try {
+            socket = (SSLSocket) socketFactory.createSocket(auditContext.getAuditRepositoryAddress(),
+                    auditContext.getAuditRepositoryPort());
+            if (socketTestPolicy != SocketTestPolicy.DONT_TEST_POLICY) {
+                // Need to perform the SSL handshake before we set the aggressive SO_TIMEOUT,
+                // otherwise the handshake will fail with a read-timeout.
+                // Javadoc: "This method is synchronous for the initial handshake on a
+                // connection [..]."
+                socket.startHandshake();
+                socket.setSoTimeout(MIN_SO_TIMEOUT);
+            }
+        } catch (IOException e) {
+            throw new AuditException(String.format("Could not establish TLS connection to %s:%d",
+                    auditContext.getAuditRepositoryAddress().getHostAddress(), auditContext.getAuditRepositoryPort()),
+                    e);
+        }
 
+        return socket;
+    }
+
+    /**
+     * Tries to determine whether the socket connection is alive and well by reading
+     * from the socket's {@link InputStream input stream}. Since syslog is a simplex
+     * protocol the results of the read can either be
+     * <dl>
+     * <dt>SocketTimeoutException</dt>
+     * <dd>We assume the connection is alive</dd>
+     * <dt>Read {@code -1}</dt>
+     * <dd>Server closed connection</dd>
+     * <dt>IOException</dt>
+     * <dd>We assume the connection is dead</dd>
+     * </dl>
+     * 
+     * @param socket The socket (connection) under test
+     * @return {@code true} if the connection is alive, {@code false} otherwise
+     */
+    private boolean isSocketConnectionAlive(final Socket socket) {
+        boolean isAlive;
+
+        try {
+            if (socket.getSoTimeout() > 0) {
+                final int nextByte = socket.getInputStream().read();
+                if (nextByte > -1) {
+                    LOG.warn(
+                            "Socket test was able to read a byte from the socket other than the 'stream closed' value of -1. "
+                                    + "This should never happen since SYSLOG is a simplex (write only) protocol! Byte value read from stream: {}",
+                            nextByte);
+                    isAlive = true;
+                } else {
+                    LOG.debug("Socket test read '-1' -> connection closed by server.");
+                    isAlive = false;
+                }
+            } else {
+                throw new IllegalStateException("Test requires an SO_TIMEOUT greater than zero set on the socket.");
+            }
+        } catch (SocketTimeoutException e) {
+            LOG.debug("Socket read timed out; assuming the connection is still alive.");
+            isAlive = true;
+        } catch (IOException e) {
+            LOG.warn("Socket read failed for non-timeout reason; assuming the connection is dead.", e);
+            isAlive = false;
+        }
+
+        return isAlive;
+    }
+
+    /**
+     * Closes socket if it is not null and has not been closed yet.
+     * 
+     * @param socket Socket to close.
+     */
+    private void closeSocket(final Socket socket) {
+        if (socket != null && !socket.isClosed()) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                LOG.debug(
+                        "Failed to close pre-existing socket. As we are either shutting down or are in the process of replacing the socket this is not really a worry... Message: {}",
+                        e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Enum to control the level of paranoia when it comes to trusting the socket
+     * connection. The more paranoid the more overhead is incurred.
+     * 
+     * @author taastrad
+     */
+    public static enum SocketTestPolicy {
+
+        DONT_TEST_POLICY(false, false),
+        TEST_BEFORE_WRITE(true, false),
+        TEST_AFTER_WRITE(false, true),
+        TEST_BEFORE_AND_AFTER_WRITE(true, true);
+
+        private final boolean beforeWrite;
+        private final boolean afterWrite;
+
+        private SocketTestPolicy(boolean beforeWrite, boolean afterWrite) {
+            this.beforeWrite = beforeWrite;
+            this.afterWrite = afterWrite;
+        }
+
+        public boolean isBeforeWrite() {
+            return beforeWrite;
+        }
+
+        public boolean isAfterWrite() {
+            return afterWrite;
+        }
+
+    }
+
+    /**
+     * We use exceptions for control flow. Which is an anti pattern. In places where
+     * we raise the exception we can at least make sure the overhead of creating the
+     * exception is minimal by not populating the stacktrace.
+     * 
+     * @author taastrad
+     */
+    private class FastSocketException extends SocketException {
+        private static final long serialVersionUID = 1L;
+
+        public FastSocketException(final String msg) {
+            super(msg);
+        }
+
+        @Override
+        public Throwable fillInStackTrace() {
+            // Intentional NOOP
+            return null;
+        }
+    }
 }
---
+++ commons/audit/src/test/java/org/openehealth/ipf/commons/audit/protocol/TLSSyslogSenderImplTest.java
@@ -0,0 +1,203 @@
+package org.openehealth.ipf.commons.audit.protocol;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.openehealth.ipf.commons.audit.AuditContext;
+import org.openehealth.ipf.commons.audit.protocol.TLSSyslogSenderImpl.SocketTestPolicy;
+
+public class TLSSyslogSenderImplTest {
+
+	private static final String SENDING_HOST = "blah";
+	private static final String SENDING_PROCESS = "blub";
+	private static final String AUDIT_MESSAGE = "Quot erat demonstrandum!";
+	private static final Integer AUDIT_REPO_PORT = 9999;
+
+	@Mock
+	private SSLSocketFactory socketFactory;
+	@Mock
+	private SSLSocket socket;
+	@Mock
+	private OutputStream os;
+	@Mock
+	private InputStream is;
+	@Mock
+	private AuditContext auditContext;
+	@Mock
+	private InetAddress inetAddress;
+
+	private TLSSyslogSenderImpl tssi;
+
+	@Rule
+	public MockitoRule rule = MockitoJUnit.rule();
+	
+	@Before
+	public void setup() throws IOException {
+		when(auditContext.getSendingApplication()).thenReturn(SENDING_PROCESS);
+		when(auditContext.getAuditRepositoryAddress()).thenReturn(inetAddress);
+		when(auditContext.getAuditRepositoryPort()).thenReturn(AUDIT_REPO_PORT);
+		
+		when(inetAddress.getHostAddress()).thenReturn(SENDING_HOST);
+		
+		when(socketFactory.createSocket(inetAddress, AUDIT_REPO_PORT)).thenReturn(socket);
+		
+		when(socket.getOutputStream()).thenReturn(os);
+	}
+
+	@Test
+	public void sendNoReadTest() throws Exception {
+		ArgumentCaptor<byte[]> streamWriteCaptor = ArgumentCaptor.forClass(byte[].class);
+		
+		tssi = new TLSSyslogSenderImpl(SENDING_HOST, SENDING_PROCESS, socketFactory, SocketTestPolicy.DONT_TEST_POLICY);
+		tssi.send(auditContext, AUDIT_MESSAGE);
+		
+		verify(socketFactory, times(1)).createSocket(any(InetAddress.class), any(Integer.class));
+		verify(socket, never()).startHandshake();
+		verify(socket, never()).setSoTimeout(any(Integer.class));
+		verify(socket, never()).getSoTimeout();
+		// write #1: syslog frame metadata
+		// write #2: audit message
+		verify(os, times(2)).write(streamWriteCaptor.capture());
+		final String auditMessageWithPreamble =  new String(streamWriteCaptor.getAllValues().get(1), StandardCharsets.UTF_8);
+		assertTrue(auditMessageWithPreamble.endsWith(AUDIT_MESSAGE));
+		// This is what counts: The DONT_TEST_POLICY shall not trigger any reads from the inputstream
+		verify(is, never()).read();
+	}
+	
+	@Test
+	public void sendReadBeforeAndAfterSockeConnectionOKTest() throws Exception {
+		ArgumentCaptor<byte[]> streamWriteCaptor = ArgumentCaptor.forClass(byte[].class);
+		when(socket.getSoTimeout()).thenReturn(1);
+		when(socket.getInputStream()).thenReturn(is);
+		// Socket read timeout is used as signal that socket connection is alive and well. 
+		when(is.read()).thenThrow(new SocketTimeoutException());
+		
+		tssi = new TLSSyslogSenderImpl(SENDING_HOST, SENDING_PROCESS, socketFactory, SocketTestPolicy.TEST_BEFORE_AND_AFTER_WRITE);
+		tssi.send(auditContext, AUDIT_MESSAGE);
+		
+		verify(socketFactory, times(1)).createSocket(any(InetAddress.class), any(Integer.class));
+		verify(socket, times(1)).startHandshake();
+		verify(socket, times(1)).setSoTimeout(1);
+		InOrder handshakeBeforeSoTimeout = inOrder(socket);
+		handshakeBeforeSoTimeout.verify(socket).startHandshake();
+		handshakeBeforeSoTimeout.verify(socket).setSoTimeout(any(Integer.class));
+		// write #1: syslog frame metadata
+		// write #2: audit message
+		verify(os, times(2)).write(streamWriteCaptor.capture());
+		final String auditMessageWithPreamble =  new String(streamWriteCaptor.getAllValues().get(1), StandardCharsets.UTF_8);
+		assertTrue(auditMessageWithPreamble.endsWith(AUDIT_MESSAGE));
+		// This is what counts: The TEST_BEFORE_AND_AFTER_WRITE shall trigger a read before and after the write operation
+		verify(is, times(2)).read();
+	}
+	
+	@Test
+	public void sendReadBeforeAndAfterSockeConnectionClosedTest() throws Exception {
+		ArgumentCaptor<byte[]> streamWriteCaptor = ArgumentCaptor.forClass(byte[].class);
+		when(socket.getSoTimeout()).thenReturn(1);
+		when(socket.getInputStream()).thenReturn(is);
+		// Reading -1 from socket signals connection close -> new socket
+		// Socket read timeout is used as signal that socket connection is alive and well. 
+		when(is.read()).thenReturn(-1).thenThrow(new SocketTimeoutException());
+		
+		tssi = new TLSSyslogSenderImpl(SENDING_HOST, SENDING_PROCESS, socketFactory, SocketTestPolicy.TEST_BEFORE_AND_AFTER_WRITE);
+		tssi.send(auditContext, AUDIT_MESSAGE);
+		
+		// Because we simulate a closed socket connection we open two sockets in total.
+		verify(socketFactory, times(2)).createSocket(any(InetAddress.class), any(Integer.class));
+		verify(socket, times(2)).setSoTimeout(1);
+		InOrder handshakeBeforeSoTimeout = inOrder(socket);
+		handshakeBeforeSoTimeout.verify(socket).startHandshake();
+		handshakeBeforeSoTimeout.verify(socket).setSoTimeout(any(Integer.class));
+		handshakeBeforeSoTimeout.verify(socket).startHandshake();
+		handshakeBeforeSoTimeout.verify(socket).setSoTimeout(any(Integer.class));
+		// write #1: syslog frame metadata
+		// write #2: audit message
+		verify(os, times(2)).write(streamWriteCaptor.capture());
+		final String auditMessageWithPreamble =  new String(streamWriteCaptor.getAllValues().get(1), StandardCharsets.UTF_8);
+		assertTrue(auditMessageWithPreamble.endsWith(AUDIT_MESSAGE));
+		// On the first read/test we discover the connection has been closed -> triggers fall back loop, new socket connection
+		// is opened and then we succeed on the second attempt with the normal flow of one test before and one after the write
+		// operation.
+		verify(is, times(3)).read();
+	}
+	
+	@Test
+	public void sendReadBeforeSockeConnectionOKTest() throws Exception {
+		ArgumentCaptor<byte[]> streamWriteCaptor = ArgumentCaptor.forClass(byte[].class);
+		when(socket.getSoTimeout()).thenReturn(1);
+		when(socket.getInputStream()).thenReturn(is);
+		// Socket read timeout is used as signal that socket connection is alive and well. 
+		when(is.read()).thenThrow(new SocketTimeoutException());
+		
+		tssi = new TLSSyslogSenderImpl(SENDING_HOST, SENDING_PROCESS, socketFactory, SocketTestPolicy.TEST_BEFORE_WRITE);
+		tssi.send(auditContext, AUDIT_MESSAGE);
+		
+		verify(socketFactory, times(1)).createSocket(any(InetAddress.class), any(Integer.class));
+		verify(socket, times(1)).setSoTimeout(1);
+		InOrder handshakeBeforeSoTimeout = inOrder(socket);
+		handshakeBeforeSoTimeout.verify(socket).startHandshake();
+		handshakeBeforeSoTimeout.verify(socket).setSoTimeout(any(Integer.class));
+		// write #1: syslog frame metadata
+		// write #2: audit message
+		verify(os, times(2)).write(streamWriteCaptor.capture());
+		final String auditMessageWithPreamble =  new String(streamWriteCaptor.getAllValues().get(1), StandardCharsets.UTF_8);
+		assertTrue(auditMessageWithPreamble.endsWith(AUDIT_MESSAGE));
+		// This is what counts: The TEST_BEFORE_WRITE shall trigger a read before the write operation.
+		// Matter of fact, we cannot tell when the read happened. We just have to assume at this point.
+		verify(is, times(1)).read();
+	}
+	
+	@Test
+	public void sendReadBeforeAndAfterSockeConnectionDeadTest() throws Exception {
+		ArgumentCaptor<byte[]> streamWriteCaptor = ArgumentCaptor.forClass(byte[].class);
+		when(socket.getSoTimeout()).thenReturn(1);
+		when(socket.getInputStream()).thenReturn(is);
+		// On first read we throw an IOException to signal a broken socket connection -> new socket
+		// Socket read timeout is used as signal that socket connection is alive and well. 
+		when(is.read()).thenThrow(new IOException()).thenThrow(new SocketTimeoutException());
+		
+		tssi = new TLSSyslogSenderImpl(SENDING_HOST, SENDING_PROCESS, socketFactory, SocketTestPolicy.TEST_BEFORE_AND_AFTER_WRITE);
+		tssi.send(auditContext, AUDIT_MESSAGE);
+		
+		// Because we simulate a closed socket connection we open two sockets in total.
+		verify(socketFactory, times(2)).createSocket(any(InetAddress.class), any(Integer.class));
+		verify(socket, times(2)).setSoTimeout(1);
+		InOrder handshakeBeforeSoTimeout = inOrder(socket);
+		handshakeBeforeSoTimeout.verify(socket).startHandshake();
+		handshakeBeforeSoTimeout.verify(socket).setSoTimeout(any(Integer.class));
+		handshakeBeforeSoTimeout.verify(socket).startHandshake();
+		handshakeBeforeSoTimeout.verify(socket).setSoTimeout(any(Integer.class));
+		// write #1: syslog frame metadata
+		// write #2: audit message
+		verify(os, times(2)).write(streamWriteCaptor.capture());
+		final String auditMessageWithPreamble =  new String(streamWriteCaptor.getAllValues().get(1), StandardCharsets.UTF_8);
+		assertTrue(auditMessageWithPreamble.endsWith(AUDIT_MESSAGE));
+		// On the first read/test we discover the connection has been closed -> triggers fall back loop, new socket connection
+		// is opened and then we succeed on the second attempt with the normal flow of one test before and one after the write
+		// operation.
+		verify(is, times(3)).read();
+	}
+}
