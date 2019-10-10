e2e9fbdc67cff6e0930cdb4f8476e5edb88445e0
kilianlorenz <kilian.lorenz@gmx.de>
2019-10-10 15:39:34 +0200

    Update AuditUtils.java
    Fix duplicate constant value.

--- commons/audit/src/main/java/org/openehealth/ipf/commons/audit/utils/AuditUtils.java
+++ commons/audit/src/main/java/org/openehealth/ipf/commons/audit/utils/AuditUtils.java
@@ -37,7 +37,7 @@ public class AuditUtils {
 
     private static final String PID = "PID";
     private static final String IP = "IP";
-    private static final String HOST = "IP";
+    private static final String HOST = "HOST";
     private static final String USER = "USER";
 
     /**
