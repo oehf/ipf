1651456e2135b95d79e5c451b7f96324a0047d45
Eugen Fischer <eugen.fischer@icw.de>
2020-02-05 22:09:55 +0100

    Enhance atna log events to RAD-69 and RAD-75.
    According the spec (IHE_RAD_TF_Vol3), on Retrieve Imaging Document Set [RAD-69] and Cross Gateway Retrieve Imaging Document Set [RAD-75] the EventCodeType DICOM Instances Accessed should be used.
    
    (cherry picked from commit 9feeb816bc09d05c6d6c00171fd7f8ea7c0aa939)

---
+++ commons/ihe/core/src/main/java/org/openehealth/ipf/commons/ihe/core/atna/event/ImageAccessAuditBuilder.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.openehealth.ipf.commons.ihe.core.atna.event;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.openehealth.ipf.commons.audit.AuditContext;
+import org.openehealth.ipf.commons.audit.codes.EventActionCode;
+import org.openehealth.ipf.commons.audit.codes.EventOutcomeIndicator;
+import org.openehealth.ipf.commons.audit.codes.ParticipantObjectDataLifeCycle;
+import org.openehealth.ipf.commons.audit.codes.ParticipantObjectTypeCode;
+import org.openehealth.ipf.commons.audit.codes.ParticipantObjectTypeCodeRole;
+import org.openehealth.ipf.commons.audit.event.DicomInstancesAccessedBuilder;
+import org.openehealth.ipf.commons.audit.model.TypeValuePairType;
+import org.openehealth.ipf.commons.audit.types.EventType;
+import org.openehealth.ipf.commons.audit.types.ParticipantObjectIdType;
+import org.openehealth.ipf.commons.audit.types.PurposeOfUse;
+import org.openehealth.ipf.commons.ihe.core.atna.AuditDataset;
+
+/**
+ * Builder for building IHE-specific DataExport events. It automatically sets the AuditSource, local and remote
+ * ActiveParticipant and a Human Requestor and provides methods for adding patient IDs.
+ *
+ * @author Christian Ohr
+ * @author Eugen Fischer
+ * @since 3.5
+ */
+public class ImageAccessAuditBuilder<T extends ImageAccessAuditBuilder<T>>
+        extends IHEAuditMessageBuilder<T, DicomInstancesAccessedBuilder> {
+
+    public ImageAccessAuditBuilder(final AuditContext auditContext,
+            final AuditDataset auditDataset,
+            final EventActionCode eventActionCode,
+            final EventType eventType,
+            final PurposeOfUse... purposesOfUse) {
+        this(auditContext, auditDataset, auditDataset.getEventOutcomeIndicator(),
+                auditDataset.getEventOutcomeDescription(),
+                eventActionCode, eventType, purposesOfUse);
+    }
+
+    public ImageAccessAuditBuilder(final AuditContext auditContext,
+            final AuditDataset auditDataset,
+            final EventOutcomeIndicator eventOutcomeIndicator,
+            final String eventOutcomeDescription,
+            final EventActionCode eventActionCode,
+            final EventType eventType,
+            final PurposeOfUse... purposesOfUse) {
+        super(auditContext, new DicomInstancesAccessedBuilder(
+                eventOutcomeIndicator,
+                eventOutcomeDescription,
+                eventActionCode,
+                eventType,
+                purposesOfUse));
+
+        // First the source, then the destination
+        if (auditDataset.isServerSide()) {
+            setRemoteParticipant(auditDataset);
+            addHumanRequestor(auditDataset);
+            setLocalParticipant(auditDataset);
+        } else {
+            setLocalParticipant(auditDataset);
+            addHumanRequestor(auditDataset);
+            setRemoteParticipant(auditDataset);
+        }
+    }
+
+    public T setPatient(final String patientId) {
+        if (patientId != null) {
+            delegate.addPatientParticipantObject(
+                    patientId,
+                    null,
+                    Collections.emptyList(),
+                    null);
+        }
+        return self();
+    }
+
+    public T addExportedEntity(
+            final String objectId,
+            final ParticipantObjectIdType participantObjectIdType,
+            final ParticipantObjectTypeCodeRole participantObjectTypeCodeRole,
+            final List<TypeValuePairType> details) {
+        return addExportedEntity(
+                objectId,
+                participantObjectIdType,
+                ParticipantObjectTypeCode.System,
+                participantObjectTypeCodeRole,
+                null,
+                details);
+    }
+
+    public T addExportedEntity(
+            final String objectId,
+            final ParticipantObjectIdType participantObjectIdType,
+            final ParticipantObjectTypeCode participantObjectTypeCode,
+            final ParticipantObjectTypeCodeRole participantObjectTypeCodeRole,
+            final ParticipantObjectDataLifeCycle participantObjectDataLifeCycle,
+            final List<TypeValuePairType> details) {
+        delegate.addParticipantObjectIdentification(
+                requireNonNull(participantObjectIdType, "Exported entity ID type must not be null"),
+                null,
+                null,
+                details,
+                objectId != null ? objectId : getAuditContext().getAuditValueIfMissing(),
+                participantObjectTypeCode,
+                participantObjectTypeCodeRole,
+                participantObjectDataLifeCycle,
+                null);
+        return self();
+    }
+
+    @Override
+    public void validate() {
+        super.validate();
+    }
+}
---
+++ commons/ihe/xds/src/main/java/org/openehealth/ipf/commons/ihe/xds/core/audit/event/ImageAccessBuilder.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.openehealth.ipf.commons.ihe.xds.core.audit.event;
+
+import java.util.Collections;
+import java.util.stream.IntStream;
+
+import org.openehealth.ipf.commons.audit.AuditContext;
+import org.openehealth.ipf.commons.audit.codes.EventActionCode;
+import org.openehealth.ipf.commons.audit.codes.EventOutcomeIndicator;
+import org.openehealth.ipf.commons.audit.codes.ParticipantObjectIdTypeCode;
+import org.openehealth.ipf.commons.audit.codes.ParticipantObjectTypeCodeRole;
+import org.openehealth.ipf.commons.audit.types.EventType;
+import org.openehealth.ipf.commons.audit.types.PurposeOfUse;
+import org.openehealth.ipf.commons.ihe.core.atna.AuditDataset;
+import org.openehealth.ipf.commons.ihe.core.atna.event.ImageAccessAuditBuilder;
+import org.openehealth.ipf.commons.ihe.xds.core.audit.XdsNonconstructiveDocumentSetRequestAuditDataset;
+import org.openehealth.ipf.commons.ihe.xds.core.audit.XdsSubmitAuditDataset;
+
+/**
+ * @author Christian Ohr
+ * @quthor Eugen Fischer
+ * @since 3.5
+ */
+public class ImageAccessBuilder extends ImageAccessAuditBuilder<ImageAccessBuilder> {
+
+    public ImageAccessBuilder(final AuditContext auditContext,
+            final AuditDataset auditDataset,
+            final EventOutcomeIndicator eventOutcomeIndicator,
+            final String eventOutcomeDescription,
+            final EventActionCode eventActionCode,
+            final EventType eventType,
+            final PurposeOfUse... purposesOfUse) {
+        super(auditContext, auditDataset, eventOutcomeIndicator, eventOutcomeDescription, eventActionCode, eventType,
+                purposesOfUse);
+    }
+
+    public ImageAccessBuilder setSubmissionSet(final XdsSubmitAuditDataset auditDataset) {
+        return addExportedEntity(auditDataset.getSubmissionSetUuid(),
+                ParticipantObjectIdTypeCode.XdsMetadata,
+                ParticipantObjectTypeCodeRole.Job,
+                Collections.emptyList());
+    }
+
+    public ImageAccessBuilder setSubmissionSetWithHomeCommunityId(final XdsSubmitAuditDataset auditDataset,
+            final boolean xcaHomeCommunityId) {
+        return addExportedEntity(auditDataset.getSubmissionSetUuid(),
+                ParticipantObjectIdTypeCode.XdsMetadata,
+                ParticipantObjectTypeCodeRole.Job,
+                documentDetails(null, auditDataset.getHomeCommunityId(), null, null, xcaHomeCommunityId));
+    }
+
+    public ImageAccessBuilder addDocumentIds(final XdsNonconstructiveDocumentSetRequestAuditDataset auditDataset,
+            final XdsNonconstructiveDocumentSetRequestAuditDataset.Status status,
+            final boolean xcaHomeCommunityId) {
+        final String[] documentIds = auditDataset.getDocumentIds(status);
+        final String[] homeCommunityIds = auditDataset.getHomeCommunityIds(status);
+        final String[] repositoryIds = auditDataset.getRepositoryIds(status);
+        final String[] seriesInstanceIds = auditDataset.getSeriesInstanceIds(status);
+        final String[] studyInstanceIds = auditDataset.getStudyInstanceIds(status);
+        IntStream.range(0, documentIds.length).forEach(i ->
+                addExportedEntity(
+                        documentIds[i],
+                        ParticipantObjectIdTypeCode.ReportNumber,
+                        ParticipantObjectTypeCodeRole.Report,
+                        documentDetails(repositoryIds[i], homeCommunityIds[i], seriesInstanceIds[i],
+                                studyInstanceIds[i], xcaHomeCommunityId)));
+        return self();
+    }
+
+}
