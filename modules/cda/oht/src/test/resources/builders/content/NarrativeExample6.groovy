3dc99a623087977d189d22c0c8f81c206485a30a
Christian Ohr <christian.ohr@gmail.com>
2020-04-09 15:48:01 +0200

    some dependency updates, remove all site.xml files of the old documentation generation

--- modules/cda/oht/src/test/resources/builders/content/NarrativeExample6.groovy
+++
@@ -1,6 +0,0 @@
-package builders.content
-
-strucDocText('Erythematous rash, palmar surface, left index finger.') {
-	renderMultiMedia(referencedObject: ['MM1', 'MM2'])  // multi-valued primitive attribute
-}
-
