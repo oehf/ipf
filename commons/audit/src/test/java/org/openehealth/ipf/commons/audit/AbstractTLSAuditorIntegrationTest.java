09e392a5b40ea85955c672c957faf78967390660
Thomas Papke <web@thopap.de>
2021-05-17 21:09:55 +0200

    Slightly increase timeout to avoid issue on slower maschines

--- commons/audit/src/test/java/org/openehealth/ipf/commons/audit/AbstractTLSAuditorIntegrationTest.java
+++ commons/audit/src/test/java/org/openehealth/ipf/commons/audit/AbstractTLSAuditorIntegrationTest.java
@@ -61,7 +61,7 @@ abstract class AbstractTLSAuditorIntegrationTest extends AbstractAuditorIntegrat
         try (var ignored = new TlsSyslogServer(consumer, Throwable::printStackTrace, defaultTls)
                 .start("localhost", port)) {
             IntStream.range(0, count).forEach(i -> executor.execute(() -> sendAudit(Integer.toString(i))));
-            boolean completed = consumer.await(10, TimeUnit.SECONDS);
+            boolean completed = consumer.await(15, TimeUnit.SECONDS);
             assertTrue("Consumer only received " + consumer.getSyslogEvents().size(), completed);
         } finally {
             executor.shutdownNow();
