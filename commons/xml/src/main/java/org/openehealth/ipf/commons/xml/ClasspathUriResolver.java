d64e142b8ba04354f15a8bc1141c408e911c7908
Quentin Ligier <quentin.ligier@bluewin.ch>
2022-10-07 13:24:23 +0200

    Avoid SAXSources in ClasspathUriResolver

--- commons/xml/src/main/java/org/openehealth/ipf/commons/xml/ClasspathUriResolver.java
+++ commons/xml/src/main/java/org/openehealth/ipf/commons/xml/ClasspathUriResolver.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,55 +21,41 @@ import javax.xml.transform.URIResolver;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamSource;
 
-import org.xml.sax.InputSource;
-
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 /**
- * URIResolver used to correctly resolve import commands in xslt or xquery
- * content. The referenced resource (stylesheet or document) can be found
- * somewhere in the classloader's classpath. If it can't be found, the default
- * URIResolver is used.
- * 
+ * URIResolver used to correctly resolve import commands in xslt or xquery content. The referenced resource (stylesheet
+ * or document) can be found somewhere in the classloader's classpath. If it can't be found, the default URIResolver is
+ * used.
+ *
  * @author Christian Ohr
  */
 class ClasspathUriResolver implements URIResolver {
 
     private final URIResolver standardResolver;
-    
+
     public ClasspathUriResolver(URIResolver resolver) {
         super();
         standardResolver = resolver;
     }
 
     /**
-     * Resolve by searching the classpath, fallback to default resolution
-     * strategy.
-     * 
+     * Resolve by searching the classpath, fallback to default resolution strategy.
+     * <p>
+     * Avoid using {@link SAXSource} because Saxon's {@link net.sf.saxon.lib.EntityResolverWrappingResourceResolver}
+     * doesn't support it.
      */
     @Override
     public Source resolve(String href, String base) throws TransformerException {
-        var cl = getClass().getClassLoader();
-        var url = cl.getResource(href);
-        if (url != null) {
-            var saxSource = new SAXSource();
-            saxSource.setInputSource(new InputSource(url.toString()));
-            saxSource.setSystemId(url.toString());
-            return saxSource;
-        } else if (standardResolver != null) {
-            return standardResolver.resolve(href, base);
-        } else if (href != null && base != null) {
-            final URI baseUri;
-            try {
-                baseUri = new URI(base);
-            } catch (final URISyntaxException e) {
-                return null;
+        final var inputStream = getClass().getClassLoader().getResourceAsStream(href);
+        if (inputStream != null) {
+            return new StreamSource(inputStream);
+        } else {
+            final var source = standardResolver.resolve(href, base);
+            if (source instanceof SAXSource) {
+                return new StreamSource(((SAXSource) source).getInputSource().getByteStream());
+            } else {
+                return source;
             }
-            return new StreamSource(new File(baseUri.resolve(href)));
         }
-        return null;
     }
 
 }
