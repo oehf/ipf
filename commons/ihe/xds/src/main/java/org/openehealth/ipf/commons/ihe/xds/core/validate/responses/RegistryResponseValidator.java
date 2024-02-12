1523a3138992889bfcb56441c67817db6583479c
Dmytro Rud <Dmytro.Rud@adesso.ch>
2024-02-12 22:08:39 +0100

    #440: fixed validation of XDS response attributes

--- commons/ihe/xds/src/main/java/org/openehealth/ipf/commons/ihe/xds/core/validate/responses/RegistryResponseValidator.java
+++ commons/ihe/xds/src/main/java/org/openehealth/ipf/commons/ihe/xds/core/validate/responses/RegistryResponseValidator.java
@@ -16,7 +16,7 @@
 package org.openehealth.ipf.commons.ihe.xds.core.validate.responses;
 
 import org.openehealth.ipf.commons.core.modules.api.Validator;
-import org.openehealth.ipf.commons.ihe.xds.XdsIntegrationProfile;
+import org.openehealth.ipf.commons.ihe.xds.XCA;
 import org.openehealth.ipf.commons.ihe.xds.core.ebxml.EbXMLRegistryResponse;
 import org.openehealth.ipf.commons.ihe.xds.core.validate.HomeCommunityIdValidator;
 import org.openehealth.ipf.commons.ihe.xds.core.validate.ValidationProfile;
@@ -46,7 +46,7 @@ public class RegistryResponseValidator implements Validator<EbXMLRegistryRespons
             metaDataAssert(registryError.getErrorCode() != null, INVALID_ERROR_CODE_IN_RESPONSE);
             metaDataAssert(registryError.getSeverity() != null, INVALID_SEVERITY_IN_RESPONSE);
 
-            if (profile.getInteractionProfile().getHomeCommunityIdOptionality() != XdsIntegrationProfile.HomeCommunityIdOptionality.NEVER) {
+            if ((profile.getInteractionId() == XCA.Interactions.ITI_38) || (profile.getInteractionId() == XCA.Interactions.ITI_39)) {
                 hcValidator.validate(registryError.getLocation());
             }
         }
