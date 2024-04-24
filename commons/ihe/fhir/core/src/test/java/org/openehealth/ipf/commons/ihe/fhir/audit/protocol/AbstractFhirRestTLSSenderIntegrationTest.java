35e70d385d9fef8377db9d57f971bc5a403bd49a
Thomas Papke <web@thopap.de>
2024-04-24 21:20:38 +0200

    #433: Try to fix build failure on JDK21 due to different test order

--- commons/ihe/fhir/core/src/test/java/org/openehealth/ipf/commons/ihe/fhir/audit/protocol/AbstractFhirRestTLSSenderIntegrationTest.java
+++ commons/ihe/fhir/core/src/test/java/org/openehealth/ipf/commons/ihe/fhir/audit/protocol/AbstractFhirRestTLSSenderIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openehealth.ipf.commons.audit.CustomTlsParameters;
 import org.openehealth.ipf.commons.audit.DefaultBalpAuditContext;
+import org.openehealth.ipf.commons.audit.FhirContextHolder;
 import org.openehealth.ipf.commons.audit.TlsParameters;
 import org.openehealth.ipf.commons.audit.codes.EventOutcomeIndicator;
 import org.openehealth.ipf.commons.audit.event.ApplicationActivityBuilder;
@@ -29,6 +30,7 @@ public abstract class AbstractFhirRestTLSSenderIntegrationTest {
 
     @BeforeEach
     public void setup() {
+        FhirContextHolder.remove();
         this.auditContext = new DefaultBalpAuditContext();
         auditContext.setAuditRepositoryPort(FhirAuditRepository.getServerHttpsPort());
         auditContext.setAuditRepositoryContextPath(FhirAuditRepository.getServerContextPath());
